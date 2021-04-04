a797de4bc055bf209794170934184ce1d7dc11c2
JonAnder33 <jonander.delapuebla@opendeusto.es>
2021-04-04 11:54:17 +0200

    Añadido transladar

--- Examen2014/punto.c
+++ Examen2014/punto.c
@@ -9,21 +9,24 @@
 #include <stdio.h>
 #include <math.h>
 
-void imprimirPunto(Punto p)
-{
+void imprimirPunto(Punto p) {
 	printf("(%i, %i)\n", p.x, p.y);
 }
 
-float distancia(Punto p1, Punto p2){
+float distancia(Punto p1, Punto p2) {
 	float distancia;
-	distancia=sqrt((p2.x-p1.x)*(p2.x-p1.x)+(p2.y-p1.y)*(p2.y-p1.y));
+	distancia = sqrt(
+			(p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
 	return distancia;
 }
 
-void trasladarXY(Punto p1, Punto p2, int x, int y){
-	p1.x=p1.x+x;
-	p1.y=p1.y+y;
-	p2.x=p2.x+x;
-	p2.y=p2.y+y;
+void trasladarXY(Punto *p1, Punto *p2, int x, int y) {
+
+	p1->x += x;
+
+	p2->x += x;
+	p1->y += y;
+	p2->y += y;
+
 	return;
 }

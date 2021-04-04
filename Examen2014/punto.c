b4cce6a10e0cbb3bf9b8a9b6cad5784afba8a2f6
JonAnder33 <jonander.delapuebla@opendeusto.es>
2021-04-04 11:40:07 +0200

    Clase punto añadida los metodos

--- Examen2014/punto.c
+++ Examen2014/punto.c
@@ -7,7 +7,7 @@
 
 #include "punto.h"
 #include <stdio.h>
-
+#include <math.h>
 
 void imprimirPunto(Punto p)
 {
@@ -15,10 +15,15 @@ void imprimirPunto(Punto p)
 }
 
 float distancia(Punto p1, Punto p2){
-	//TODO
-	return 0;
+	float distancia;
+	distancia=sqrt((p2.x-p1.x)^2+(p2.y-p1.y)^2);
+	return distancia;
 }
 
 void trasladarXY(Punto p1, Punto p2, int x, int y){
-	//TODO
+	p1.x=p1.x+x;
+	p1.y=p1.y+y;
+	p2.x=p2.x+x;
+	p2.y=p2.y+y;
+	return;
 }

c0161041614e4903d8943c41740bc9e9c9ebae84
danel <danel@DESKTOP-3DCJVBQ>
2021-04-04 11:54:59 +0200

    main basico

--- Examen2014/main.c
+++ Examen2014/main.c
@@ -5,9 +5,22 @@
  *      Author: danel y jonander
  */
 #include "punto.h"
+#include <stdio.h>
+
 int main(int argc, char **argv) {
 	Punto p1 = { 1, 2 };
+	Punto p2 = { 3, 4 };
 	imprimirPunto(p1);
+	imprimirPunto(p2);
+
+	printf("Distancia: %f\n",distancia(p1,p2));
+
+	trasladarXY(&p1,&p2,5,6);
+
+	printf("Despues de trasladar...\n");
+	printf("Distancia: %f\n",distancia(p1,p2));
+	imprimirPunto(p1);
+	imprimirPunto(p2);
 
 	return 0;
 }
